f51e12450221858faa6cc0ec5f5d74d6a8249899
Leon <leon.karho.chung@gmail.com>
2019-03-13 17:12:06 -0400

    assign className values to Object keys that match with value of each hour.hourIcon

--- client/src/components/NextTwentyFour.js
+++ client/src/components/NextTwentyFour.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
 const NextTwentyFour = ({ hours }) => {
+  const currentIconObj = {
+    clearday: 'uil uil-sun',
+    clearnight: 'uil uil-moonset',
+    rain: 'uil uil-cloud-showers',
+    snow: 'uil uil-snowflake',
+    sleet: 'uil uil-cloud-hail',
+    wind: 'uil uil-wind',
+    fog: 'uil uil-cloud-set',
+    cloudy: 'uil uil-clouds',
+    partlycloudyday: 'uil uil-cloud-sun',
+    partlycloudynight: 'uil uil-cloud-moon'
+  }
   return (
     <div className="hourly-block">
       {hours.map((hour, index) => {
@@ -8,7 +20,7 @@ const NextTwentyFour = ({ hours }) => {
           <div key={index} className="hourly-block-items">
             <h3>{hour.tempInC}&deg;</h3>
             <h3>
-              <i className="uil uil-snowflake" />
+              <i className={currentIconObj[hour.hourIcon]} />
             </h3>
             <h3>{hour.currentTime}:00</h3>
           </div>
